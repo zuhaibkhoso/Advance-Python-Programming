4567cdcca501cc73461b1f96c1612cdcd5b4f5c9
unknown <90856997+zuhaibkhoso@users.noreply.github.com>
2025-03-27 17:03:43 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -6,26 +6,31 @@ while True:
 
     user_input = input("Operation: ")
 
+    # do to addition
     if user_input == "+":
         num1 = float(input("Enter 1st Number: "))
         num2 = float(input("Enter 2nd Number: "))
         print(f"\n{num1} + {num2} = {num1 + num2}")
 
+    # do to subtraction
     elif user_input == "-":
         num1 = float(input("Enter 1st Number: "))
         num2 = float(input("Enter 2nd Number: "))
         print(f"\n{num1} - {num2} = {num1 - num2}")
 
+    # do to multiplication
     elif user_input == "*":
         num1 = float(input("Enter 1st Number: "))
         num2 = float(input("Enter 2nd Number: "))
         print(f"\n{num1} x {num2} = {num1 * num2}")
 
+    # do to division
     elif user_input == "/":
         num1 = float(input("Enter 1st Number: "))
         num2 = float(input("Enter 2nd Number: "))
         print(f"\n{num1} / {num2} = {num1 / num2}")
 
+    # invalid input && quit system
     elif user_input != sign and user_input == "-q":
         print("\nQuit!")
         break
